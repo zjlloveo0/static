f56463f7a087e2933d2544d4acb2b5ed6d81a908
周金龙 <mrzhou5@126.com>
2020-03-04 16:47:37 +0800

    a

--- pay/js/qr.js
+++ pay/js/qr.js
@@ -1,16 +1,16 @@
-    // $(document).ready(function(){ 
-    // $(document).bind("contextmenu",function(e){return false;});});
-    // $(document).ready(function() {
-    // $(document).bind("keydown",function(e){e=window.event||e; if(e.keyCode==116){e.keyCode = 0;return  false;} }); }); 
-    // $(document).ready(function() {
-    // $(document).bind("keydown",function(e){e=window.event||e; if(e.keyCode==118){e.keyCode = 0;return  false;} }); });
-    // $(document).ready(function() {
-    // $(document).bind("keydown",function(e){e=window.event||e; if(e.keyCode==122){e.keyCode = 0;return  false;} }); });
-    // $(document).ready(function() {
-    // $(document).bind("keydown",function(e){e=window.event||e; if(e.keyCode==123){e.keyCode = 0;return  false;} }); });
-    // $(document).ready(function() {
-    // $(document).bind("keydown",function(e){e=window.event||e; if(e.keyCode==83){e.keyCode = 0;return  false;} }); });
-    console.log('%c', "padding:180px 180px;line-height:400px;background:url('https://ww2.sinaimg.cn/large/a15b4afegy1fo1tmhcqebj209f09ymxi.jpg') no-repeat;", "\nHey man! tealing code again！\nWhy don't you buy one?\nAdd QQ774740085\n\n");
+    $(document).ready(function(){ 
+    $(document).bind("contextmenu",function(e){return false;});});
+    $(document).ready(function() {
+    $(document).bind("keydown",function(e){e=window.event||e; if(e.keyCode==116){e.keyCode = 0;return  false;} }); }); 
+    $(document).ready(function() {
+    $(document).bind("keydown",function(e){e=window.event||e; if(e.keyCode==118){e.keyCode = 0;return  false;} }); });
+    $(document).ready(function() {
+    $(document).bind("keydown",function(e){e=window.event||e; if(e.keyCode==122){e.keyCode = 0;return  false;} }); });
+    $(document).ready(function() {
+    $(document).bind("keydown",function(e){e=window.event||e; if(e.keyCode==123){e.keyCode = 0;return  false;} }); });
+    $(document).ready(function() {
+    $(document).bind("keydown",function(e){e=window.event||e; if(e.keyCode==83){e.keyCode = 0;return  false;} }); });
+    console.log('%c', "padding:180px 180px;line-height:400px;background:url('http://useronline-1251779293.image.myqcloud.com/headimg/1329345066078126081554603053048.png') no-repeat;", "\nHey man! tealing code again！\nWhy don't you buy one?\nAdd QQ1357423913\n\n");
     function urlEncode(String) {
         return encodeURIComponent(String).replace(/'/g,"%27").replace(/"/g,"%22");  
     }
